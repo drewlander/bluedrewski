054ae83ca8df329d11c9ad08a8460d74025b1297
drewski <drewskiwooskie@gmail.com>
2024-12-09 16:23:18 -0500

    does this work

---
+++ files/scripts/enableautoupdate.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+systemctl enable rpm-ostreed-automatic.timer
+systemctl enable podman-auto-update.timer
+systemctl --global enable podman-auto-update.timer
+
