dfd4e49c5896bc6da670b12c90bcf9e885cfd904
drewski <drewskiwooskie@gmail.com>
2024-12-09 22:55:38 -0500

    multimedia

---
+++ files/scripts/multimedia.sh
@@ -0,0 +1,5 @@
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+sudo dnf install rpmfusion-free-release-tainted
+sudo dnf install libdvdcss
