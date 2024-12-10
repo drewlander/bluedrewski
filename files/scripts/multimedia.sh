e7dabdf3a9df735719997bb33b8ef49b0edcae6c
drewski <drewskiwooskie@gmail.com>
2024-12-09 23:19:24 -0500

    put it here

--- files/scripts/multimedia.sh
+++ files/scripts/multimedia.sh
@@ -1,5 +1,6 @@
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
 sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
 sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+sudo dnf install intel-media-driver
 sudo dnf install rpmfusion-free-release-tainted
 sudo dnf install libdvdcss
